83822f3c446709e014ed85ca233de1cabb692473
Mohammed El Fraiji <114163433+momoel00@users.noreply.github.com>
2023-02-23 09:57:28 +0100

    Commit int
    Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+// document.getElementById("btn").addEventListener("click", function(event){
+
+//     let values = document.getElementById("myInput").value ; 
+//     let list =  document.getElementById("myUL");
+//     let creatElement = document.createElement("li")
+//     let creatValue = document.createTextNode(values)
+//     creatElement.appendChild(creatValue)
+//     document.getElementById("myUL").appendChild(creatElement)
+
+
+// })
+
+
+
+
+// // let allList = document.getElementById("li")
+// // if (this.click == true){
+// //   console.log("hi")
+// // }
+
+
+// document.getElementById("myUL").addEventListener("click", function(event){
+//   let clicked = true;
+//   if(clicked == true){
+//     event.target.remove();
+//   }
+// })
+
+
+// // // Get all the list items in the HTML list
+// // const listItems = document.querySelectorAll('li');
+
+// // // Loop through each list item and add a click event listener
+// // listItems.forEach((item) => {
+// //   item.addEventListener('click', (event) => {
+// //     // Remove the clicked item from the list
+// //     event.target.remove();
+// //   });
+// // });
